771a7ba6910a6aa1a2819c26e1147200dcf5da30
reuss37 <reemosama333@hotmail.com>
2023-09-07 11:26:18 -0700

    second

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program using printf function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

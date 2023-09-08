3d16e80ba751f962810c682f8e8279111ae5bbbe
reuss37 <reemosama333@hotmail.com>
2023-09-08 12:57:48 -0700

    w

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: upper then lower
+ *
+ * Return: Always 0(success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	printf("%d\n", i);
+
+	return (0);
+}

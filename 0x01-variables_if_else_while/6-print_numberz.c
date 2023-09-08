9d618b4e27e2ed975f2400704e9bf81239d35f1f
reuss37 <reemosama333@hotmail.com>
2023-09-08 13:11:12 -0700

    s

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,16 +10,10 @@
 
 int main(void)
 {
-		putchar('0');
-		putchar('1');
-		putchar('2');
-		putchar('3');
-		putchar('4');
-		putchar('5');
-		putchar('6');
-		putchar('7');
-		putchar('8');
-		putchar('9');
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
 		putchar('\n');
 		return (0);
 }

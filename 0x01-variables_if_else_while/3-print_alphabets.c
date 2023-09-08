2cc5cd3fba82bce5bf8ad6ada541810df13b6bc8
reuss37 <reemosama333@hotmail.com>
2023-09-08 12:39:13 -0700

    e

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: upper then lower
+ *
+ * Return: Always 0(success)
+*/
+
+int main(void)
+{
+	int n = 97;
+	int k = 65;
+
+	while (n >= 122)
+	{
+		putchar(n);
+	}
+	while (k <= 90)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}

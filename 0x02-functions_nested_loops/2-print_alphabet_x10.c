b044409e6b466da9f3b60cc7f585676fd1d5c9d6
reuss37 <reemosama333@hotmail.com>
2023-09-12 08:19:33 -0700

    f

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that will print alphabet 10 times
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int line;
+	int ch;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (ch = 97; ch <= 122; ch++)
+			_putchar(ch);
+		-putchar('\n');
+	}
+}

79631c7d3eb87f650079c44affb145ac47e8cc3d
reuss37 <reemosama333@hotmail.com>
2023-09-12 07:55:25 -0700

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
  * Return: Always 0 (success)
 */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int ch;
 
-	for (ch = 97, ch <= 122, ch++)
+	for (ch = 97; ch <= 122; ch++)
 		_putchar(ch);
 	_putchar('\n');
 }

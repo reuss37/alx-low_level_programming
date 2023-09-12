a588203c1181158b9e2d69fd5d0e9024cf410183
reuss37 <reemosama333@hotmail.com>
2023-09-12 09:56:57 -0700

    n

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,15 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
 	if (n == 0)
 	{
 		_putchar(48);
 		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
 	}
 }
 

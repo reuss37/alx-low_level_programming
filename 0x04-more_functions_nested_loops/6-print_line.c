95b0f5c71e3b37b11a443b49940841284240933a
reuss37 <reemosama333@hotmail.com>
2023-09-16 11:46:00 -0700

    fd

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the _ character sholud  be print
+*/
+
+void print_line(int n)
+{
+	int lnChr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnChr = 1; lnChr <= n; lnChr++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}

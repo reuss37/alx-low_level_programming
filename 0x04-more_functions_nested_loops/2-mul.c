61f74eaeeb0ec73cd638ca93de459206603ecf21
reuss37 <reemosama333@hotmail.com>
2023-09-16 09:36:41 -0700

    e

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies twi integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

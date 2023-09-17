5914341b0d4544a152174910ef99f2d80f52b0e4
reuss37 <reemosama333@hotmail.com>
2023-09-17 11:59:52 -0700

    s

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,16 +13,19 @@ void print_square(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	for (int i = 0; i < size; i++)
+	else
 	{
-		for (int j = 0; j < size; j++)
+		int i, j;
+
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
 

550c4d66b9147fb3473bbdec8f46d7c098f79ca5
reuss37 <reemosama333@hotmail.com>
2023-09-18 12:49:59 -0700

    z

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reverted string, followed by new line
+ * @s: pointer to string to print
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

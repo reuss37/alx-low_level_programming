9b860ff397ccc188eaa3f1b124d4b32416420201
reuss37 <reemosama333@hotmail.com>
2023-09-18 14:15:35 -0700

    ew

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,7 +17,7 @@ void rev_string(char *s)
 	for (l = 0; s[l] != '\0'; ++l)
 		;
 	/*swap the string by looping to half the string*/
-	for (i = 0; i < 1 / 2; i++)
+	for (i = 0; i < l / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[l - 1 - i]; /*-1 because array starts from 0*/

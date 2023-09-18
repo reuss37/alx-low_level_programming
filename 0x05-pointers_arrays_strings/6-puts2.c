ac3a11e53deab55468832b331adf4996c9aa1030
reuss37 <reemosama333@hotmail.com>
2023-09-18 14:25:47 -0700

    b

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * put82 - prints every character of string
+ *
+ * @str: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}

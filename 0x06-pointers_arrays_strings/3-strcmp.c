0ebc3527be15eb9c89d6820f75fcbb7942d62564
reuss37 <reemosama333@hotmail.com>
2023-10-04 03:01:09 -0700

    e

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,12 +2,9 @@
 
 /**
  * _strcmp - compares two strings
- * @sl: first string to compare
- * @s2: second string to compare
- * Return: 0 if strings are equal, negative value
- *				if s1 < s2,
- *				or positive value if
- *				s1 > s2
+ * @sl: string 1
+ * @s2: string 2
+ * Return: 1 if true, 0 if false
 */
 
 int _strcmp(char *s1, char *s2)
